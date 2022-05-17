eef6043668917cb6a275b50b9a40571ea2f620f5
gtsaran <74060197+gtsaran@users.noreply.github.com>
2022-05-18 00:30:01 +0530

    Update TestArray.java

--- TestArray.java
+++ TestArray.java
@@ -9,7 +9,7 @@ class TestArray {
 		System.out.println();
 		float sum = 0, avg;
 		for(int i = 0;i<a.length;i++)
-		  sum+=a[i];
+		  sum+=a[i]; 
 		avg=sum/a.length;
 		System.out.println("Average= " + avg);
 	}
