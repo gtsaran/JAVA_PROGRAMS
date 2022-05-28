28bfb29753f0b6b5a5391ac34a34793af56a14be
gtsaran <74060197+gtsaran@users.noreply.github.com>
2022-05-28 22:37:51 +0530

    Update ThirdLargestInArray.java

--- ThirdLargestInArray.java
+++ ThirdLargestInArray.java
@@ -3,7 +3,7 @@ public static int getThirdLargest(int[] a, int total){
 int temp;  
 for (int i = 0; i < total; i++)   
         {  
-            for (int j = i + 1; j < total; j++)   
+            for (int j = i + 1; j < total; j++)    
             {  
                 if (a[i] > a[j])   
                 {  
